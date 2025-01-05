b3e715e8b23d322554c469a2ca79909926a64a35
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-05 10:59:51 +0500

    post model done

---
+++ src/models/posts.models.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "description is required"],
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
