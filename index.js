717522577d663de12fe52cb0b8e29c74a5e93489
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-11 10:56:24 +0500

    send

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173", // Your React app's URL
+    credentials: true, // Allow cookies to be sent
+  }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
