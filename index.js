7505fb3ae517dd9b12f547bfce0d09f03853afba
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-01-16 16:10:54 +0500

    cors issue solving

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ import postRoutes from "./src/routes/post.routes.js";
 const app = express();
 const port = 3000;
 
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'your-production-domain.com'
+    : 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
 app.use(express.json());
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
